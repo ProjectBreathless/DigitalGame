599ce33751327f46cc8be1b04c39776cdf5ad2fb
Tristan Timblin <tatimblin@gmail.com>
2016-11-06 15:35:40 -0500

    Moved around assets

--- Game/js/Preloader.js
+++ Game/js/Preloader.js
@@ -4,11 +4,11 @@ gameObj.Preloader.prototype = {
 	preload: function() {
 		console.log("State - preload");
 		
-        this.load.tilemap('map', 'assets/tilemaps/maps/collision_test.json', null, Phaser.Tilemap.TILED_JSON);
-        this.load.image('ground_1x1', 'assets/tilemaps/tiles/ground_1x1.png');
-        this.load.image('walls_1x2', 'assets/tilemaps/tiles/walls_1x2.png');
-        this.load.image('tiles2', 'assets/tilemaps/tiles/tiles2.png');
-        this.load.spritesheet('dude', 'assets/games/starstruck/dude.png', 32, 48);
+        this.load.tilemap('map', 'levels/collision_test.json', null, Phaser.Tilemap.TILED_JSON);
+        this.load.image('ground_1x1', 'levels/ground_1x1.png');
+        this.load.image('walls_1x2', 'levels/walls_1x2.png');
+        this.load.image('tiles2', 'levels/tilemaps/tiles/tiles2.png');
+        this.load.spritesheet('dude', 'levels/dude.png', 32, 48);
         
         
 	},
