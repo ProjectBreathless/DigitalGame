8d239ff60db2f4cd65e032c581b233cad7a2e0f4
Matthew Wagar <mrw77@drexel.edu>
2016-11-16 05:32:17 -0500

    Added particle effect to rocket jump.

--- Game/js/Game.js
+++ Game/js/Game.js
@@ -36,7 +36,7 @@ gameObj.Game.prototype = {
         layer.resizeWorld();
 
 
-        map.setCollisionBetween(1, 18);
+        map.setCollisionBetween(1, 200);
 
         crystals = this.add.sprite(100, 500, 'crystal');
 
@@ -45,7 +45,7 @@ gameObj.Game.prototype = {
 
         door = this.add.sprite(1050, 270, 'door');
 
-        player = this.add.sprite(100, 400, 'Aria');
+        player = this.add.sprite(100, 300, 'Aria');
         player.animations.add('left', [0, 1, 2, 3, 4, 5], 18, true);
         //player.animations.add('turn', [4], 20, true);
         player.animations.add('right', [8, 9, 10, 11, 12, 13], 18, true);
@@ -56,11 +56,11 @@ gameObj.Game.prototype = {
         player.body.bounce.y = 0;
         this.game.physics.arcade.gravity.y = 1750;
         player.body.collideWorldBounds = true;
-        player.anchor.setTo(0.5,0.5);
+        player.anchor.setTo(0.5, 0.5);
 
         player.body.maxSpeed = 400;
         player.body.acceleration = 40;
-        player.body.aerialAcceleration = 4/7;
+        player.body.aerialAcceleration = 4 / 7;
         player.body.friction = 40;
         player.body.rocketJump = true;
         // player.body.friction = 0.5;
@@ -68,12 +68,24 @@ gameObj.Game.prototype = {
         // player.body.fixedRotation = true;
         // player.body.setMaterial(characterMaterial);
 
+        emitter = this.game.add.emitter(0, 0, 50);
+        emitter.gravity = 0;
+        emitter.bounce.setTo(0.5, 0.5);
+
+        emitter.makeParticles('rocketParticles');
+        // emitter.bounce.setTo(0.5, 0.5);
+
+        emitter.forEach(function (particle) {
+            particle.body.allowGravity = false;
+            particle.animations.add('smoke', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 28);
+        }, this);
+
         this.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
 
         cursors = this.input.keyboard.createCursorKeys();
-       a =this.game.input.keyboard.addKey(Phaser.Keyboard.A) 
-       d = this.game.input.keyboard.addKey(Phaser.Keyboard.D)
-       w = this.game.input.keyboard.addKey(Phaser.Keyboard.W)  
+        a = this.game.input.keyboard.addKey(Phaser.Keyboard.A)
+        d = this.game.input.keyboard.addKey(Phaser.Keyboard.D)
+        w = this.game.input.keyboard.addKey(Phaser.Keyboard.W)
         score = 0;
         treasure = this.add.text(32, 575, 'Treasure: ' + score.toString(), { font: "20px Arial", fill: "#ffffff", align: "left" });
 
@@ -82,8 +94,9 @@ gameObj.Game.prototype = {
     update: function () {
 
         this.game.physics.arcade.collide(player, layer);
+        this.game.physics.arcade.collide(emitter, layer);
 
-        if(this.game.input.activePointer.leftButton.isDown && player.body.rocketJump){
+        if (this.game.input.activePointer.leftButton.isDown && !player.body.onFloor() && player.body.rocketJump) {
             console.log(this.game.physics.arcade.angleToPointer(player));
             console.log(this.game.physics.arcade.distanceToPointer(player));
             player.body.velocity.x -= Math.cos(this.game.physics.arcade.angleToPointer(player)) * 500;
@@ -91,10 +104,33 @@ gameObj.Game.prototype = {
             player.body.velocity.y -= Math.sin(this.game.physics.arcade.angleToPointer(player)) * 650;
             // player.body.velocity.y = -600;
             player.body.rocketJump = false;
+
+
+
+            /*particle effect */
+            emitter.x = player.x;
+            emitter.y = player.y;
+            var t = this;
+            emitter.forEach(function (singleParticle) {
+                if(!singleParticle.animations.isPlaying){
+                    singleParticle.animations.play('smoke', 28, false, true);
+                }
+
+                // console.log(singleParticle);
+                // singleParticle.body.velocity.x = Math.cos(t.game.physics.arcade.angleToPointer(player)) * 1000w;
+                // singleParticle.body.velocity.y = Math.sin(t.game.physics.arcade.angleToPointer(player)) * 1000;
+            });
+
+            emitter.setXSpeed(Math.cos(this.game.physics.arcade.angleToPointer(player))*50 + (Math.random()-0.5)*600, Math.cos(this.game.physics.arcade.angleToPointer(player))*350 + (Math.random()-0.5)*600) 
+            
+            emitter.setYSpeed(Math.sin(this.game.physics.arcade.angleToPointer(player))*150 + (Math.random()-0.5)*600, Math.sin(this.game.physics.arcade.angleToPointer(player))*250 + (Math.random()-0.5)*600)
+            
+            emitter.start(true, 0, null, 15);
         }
+        
 
         if (a.isDown && player.body.onFloor()) {
-            if(player.body.velocity.x > -player.body.maxSpeed){
+            if (player.body.velocity.x > -player.body.maxSpeed) {
                 player.body.velocity.x -= player.body.acceleration;
             } else {
                 player.body.velocity.x = -player.body.maxSpeed
@@ -105,7 +141,7 @@ gameObj.Game.prototype = {
             }
         }
         else if (d.isDown && player.body.onFloor()) {
-            if(player.body.velocity.x < player.body.maxSpeed){
+            if (player.body.velocity.x < player.body.maxSpeed) {
                 player.body.velocity.x += player.body.acceleration;
             } else {
                 player.body.velocity.x = player.body.maxSpeed
@@ -118,10 +154,11 @@ gameObj.Game.prototype = {
         }
         else if (player.body.onFloor()) {
             /*friction */
-            
-            if(player.body.velocity.x < 0){
+            if (player.body.velocity.x > -player.body.friction && player.body.velocity.x < player.body.friction) {
+                player.body.velocity.x = 0;
+            } else if (player.body.velocity.x < 0) {
                 player.body.velocity.x += player.body.friction;
-            } else if (player.body.velocity.x > 0){
+            } else if (player.body.velocity.x > 0) {
                 player.body.velocity.x -= player.body.friction;
             }
 
@@ -141,8 +178,8 @@ gameObj.Game.prototype = {
         /* if in air: */
         else if (a.isDown) {
 
-            if(player.body.velocity.x > -player.body.maxSpeed){
-                player.body.velocity.x -= player.body.acceleration*player.body.aerialAcceleration;
+            if (player.body.velocity.x > -player.body.maxSpeed) {
+                player.body.velocity.x -= player.body.acceleration * player.body.aerialAcceleration;
             } else {
                 player.body.velocity.x = -player.body.maxSpeed
             }
@@ -154,8 +191,8 @@ gameObj.Game.prototype = {
         }
         else if (d.isDown) {
             /*Gives player opportunity to change directions, even in air*/
-            if(player.body.velocity.x < player.body.maxSpeed){
-                player.body.velocity.x += player.body.acceleration*player.body.aerialAcceleration;
+            if (player.body.velocity.x < player.body.maxSpeed) {
+                player.body.velocity.x += player.body.acceleration * player.body.aerialAcceleration;
             } else {
                 player.body.velocity.x = player.body.maxSpeed
             }
@@ -184,7 +221,7 @@ gameObj.Game.prototype = {
             //}
         }
 
-        if(player.body.onFloor()){
+        if (player.body.onFloor()) {
             player.body.rocketJump = true;
         }
 
--- Game/js/Preloader.js
+++ Game/js/Preloader.js
@@ -9,14 +9,17 @@ gameObj.Preloader.prototype = {
         this.load.spritesheet('playBtn', 'imgs/titlePlay.png', 224, 100);
         
         //Level 1
-        this.load.tilemap('map', 'levels/tiled_ninja_test.json', null, Phaser.Tilemap.TILED_JSON);
-        this.load.image('Alien_Ship_Tileset', 'levels/Alien_Ship_Tileset_2.png');
+        this.load.tilemap('map', 'levels/mwagar_tiles.json', null, Phaser.Tilemap.TILED_JSON);
+        this.load.image('Alien_Ship_Tileset', 'levels/32x32_tiles.png');
         //this.load.image('walls_1x2', 'levels/walls_1x2.png');
         //this.load.image('tiles2', 'levels/tiles2.png');
         
         this.load.spritesheet('Aria', 'levels/Aria_Spritesheet.png', 46, 72);
         this.load.image('door', 'levels/Door_locked.png');
         this.load.image('crystal', 'levels/Crystal_Sprite_Small.png')
+
+        
+        this.load.spritesheet('rocketParticles', 'levels/rocketParticle_spritesheet.png', 8, 8);
         
 	},
 	create: function() {
